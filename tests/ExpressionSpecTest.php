63223553fa9c06985c84390b81bb382ace2f68ed
Daniel Fekete <daniel.fekete@voov.hu>
2016-05-25 10:23:00 +0200

    expression chain test

--- tests/ExpressionSpecTest.php
+++ tests/ExpressionSpecTest.php
@@ -1,4 +1,7 @@
 <?php
+use voov\Spec\SpecificationBuilder;
+use voov\Spec\Specifications\Boolean\AndSpec;
+use voov\Spec\Specifications\Boolean\OrSpec;
 use voov\Spec\Specifications\ExpressionSpec;
 
 /**
@@ -26,5 +29,25 @@ class ExpressionSpecTest extends PHPUnit_Framework_TestCase
 
         $e = new ExpressionSpec("3 == o.value", ['o']);
         $this->assertTrue($e->isSatisfiedBy(['o' => $obj]));
+    }
+
+    public function testExpressionChains()
+    {
+        $obj = new stdClass();
+        $obj->value = 3;
+
+        $ret = SpecificationBuilder::build(new AndSpec(
+
+            new ExpressionSpec('o.value == 3', ['o']),
+            new ExpressionSpec('o.value > 2', ['o']),
+
+            new OrSpec(
+                new ExpressionSpec('o.value > 0', ['o']),
+                new ExpressionSpec('o.value == 0', ['o'])
+            )
+
+        ))->run(['o' => $obj]);
+
+        $this->assertTrue($ret);
     }
 }
